af0a5c57448d15c402b85f3958158d98ef11ca30
Leytek <leytek@yandex.ru>
2022-07-01 18:05:50 +0300

    feat: implement bands module

--- src/GraphQLHub.ts
+++ src/GraphQLHub.ts
@@ -2,7 +2,7 @@ import { mergeTypeDefs, mergeResolvers } from '@graphql-tools/merge';
 import { loadFilesSync } from '@graphql-tools/load-files';
 
 //import { tracksResolver } from './modules/tracks/resolvers/tracks.resolver';
-//import { bandsResolver } from './modules/bands/resolvers/bands.resolver';
+import bandsResolver from './modules/bands/resolvers/bands.resolver';
 //import { favouritesResolver } from './modules/favourites/resolvers/favourites.resolver';
 //import { genresResolver } from './modules/genres/resolvers/genres.resolver';
 import artistsResolver from './modules/artists/resolvers/artists.resolver';
@@ -11,6 +11,7 @@ import albumsResolver from './modules/albums/resolvers/albums.resolver';
 
 import { AlbumsAPI } from './modules/albums/services/albums.service';
 import { ArtistsAPI } from './modules/artists/services/artists.service';
+import { BandsAPI } from './modules/bands/services/bands.service';
 
 export default class GraphQLHub {
   public typeDefs;
@@ -19,8 +20,9 @@ export default class GraphQLHub {
   constructor() {
     const typesArray = loadFilesSync('./**/*.graphql');
     const resolversArray = [
-      albumsResolver,
-      artistsResolver,
+        albumsResolver,
+        artistsResolver,
+        bandsResolver
     ];
     this.typeDefs = mergeTypeDefs(typesArray);
     this.resolvers = mergeResolvers(resolversArray);
@@ -29,7 +31,8 @@ export default class GraphQLHub {
   public sources() {
     return {
       albumsAPI: new AlbumsAPI(),
-      artistsAPI: new ArtistsAPI()
+      artistsAPI: new ArtistsAPI(),
+      bandsAPI: new BandsAPI()
     }
   }
 }
---
+++ src/modules/bands/resolvers/bands.resolver.ts
@@ -0,0 +1,35 @@
+import { Band } from '../band';
+
+export default {
+  Query: {
+    async band(_: null, { id }: { id: string }, { dataSources }: unknown) {
+      return dataSources.bandsAPI.getOne(id);
+    },
+    async bands(_: null, { input }: unknown, { dataSources }: unknown) {
+      return (await dataSources.bandsAPI.getAll(input)).items;
+    },
+  },
+
+  Band: {
+    async genres({ genresIds }: { genresIds: [] }, _: null, { dataSources }: unknown) {
+      if (!genresIds.length) return;
+
+      return genresIds.map(async (id: string) => {
+        return await dataSources.genresAPI.getGenre(id);
+      });
+    },
+  },
+
+  Mutation: {
+    async createBand(_: null, { input }: unknown, { dataSources }: unknown) {
+      return await dataSources.bandsAPI.create(input as Band);
+    },
+    async deleteBand(_: null, { id }: { id: string }, { dataSources }: unknown
+    )  {
+      return await dataSources.bandsAPI.deleteOne(id);
+    },
+    async updateBand(_: null, { input }: unknown, { dataSources }: unknown) {
+      return await dataSources.bandsAPI.update(input as Band);
+    },
+  },
+};
